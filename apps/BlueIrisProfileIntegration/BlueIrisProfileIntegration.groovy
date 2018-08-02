f20fb269a7f691becd2404389c2a0526321f4aa6
Brian Lowrance <rayzur@rayzurbock.com>
2018-08-01 21:44:26 -0500

    Modify copyright header section

--- apps/BlueIrisProfileIntegration/BlueIrisProfileIntegration.groovy
+++ apps/BlueIrisProfileIntegration/BlueIrisProfileIntegration.groovy
@@ -2,6 +2,7 @@
  *  Blue Iris Integration
  *
  *  Copyright 2014 Tony Gutierrez
+ *  Adapted for Hubitat 8/1/2018 - Brian S. Lowrance (Rayzurbock)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -12,7 +13,6 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Adapted for Hubitat 8/1/2018 - Brian S. Lowrance
  */
 
 import java.security.MessageDigest
